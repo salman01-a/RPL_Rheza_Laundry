8734c19ec0096794551dfe38682035c9d83813a2
salman <salmanfaris10072005@gmail.com>
2025-06-04 19:09:00 +0700

    add loginpagee

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Login Page</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <div class="background-shape"></div>
+
+  <div class="login-wrapper">
+    <div class="login-container">
+      <img src="assets/logo.png" alt="Logo" class="logo-image" />
+
+      <form class="login-form">
+        <div class="input-group">
+          <input type="text" placeholder="USERNAME" required />
+        </div>
+        <div class="input-group">
+          <input type="password" placeholder="PASSWORD" required />
+        </div>
+        <button type="submit">LOGIN</button>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
